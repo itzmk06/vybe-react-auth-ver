d42a820b86c114318bc2b8886bbb5efb1f25029f
Manoj Kumar M <141392608+itzmk06@users.noreply.github.com>
2025-02-23 14:15:10 +0530

    Update axios.js

--- Vybe-main/src/api/axios.js
+++ Vybe-main/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://vybe-express-2.onrender.com',
     withCredentials: true 
 });
 
